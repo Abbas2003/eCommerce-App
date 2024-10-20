0bcebaa021fed86c27bd201bc752267a5c396228
Abbas2003 <abbas.mohammad805@gmail.com>
2024-10-20 21:26:59 +0500

    banner fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = ({ currentPage, PreviousPage }) => {
                         left: 0,
                         right: 0,
                         bottom: 0,
-                        backgroundImage: `url('https://s3-alpha-sig.figma.com/img/1461/f3d6/ff74c027a1888544144abe4be6e02cbf?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kwNqoQb4Eden1ddxvNlqKRY0IEnzP9CJ3ovx2SCpe-Pjlh6QG1t3pNlZyjE0HPz8BGQaBRAm4VArdPZZUH6F65-n5CGM1o6uU6XLQqs8q03KTg2FvrPOaD25nEoi-6rxqgHVQjT~Tx9Z154xE3Qtps2r3XwMx5RoOGgxE7qWj4DzoIYmZMst~57dDrmowr6yZnXL5vCn4fUtr44Bm8UJfTvb9fBgV4USZIDnMpiRTCU9XD6DUvO0tZYyVE7WRyrU7cZxuC0xbbdEwm7betikZMQMKHusnefCHPzmSdtwyNfDQxF6IDrKAW6chBmyFnxmtP1Onkn~QwFPQszqSCG8JQ__')`,
+                        backgroundImage: `url('/banner.png')`,
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
                         filter: 'blur(3px)', // Apply the blur only to the background image
